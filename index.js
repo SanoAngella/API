03048815c1af4e0957629bb4e6eefe98341f1dbe
Angella <sanoangella9@gmail.com>
2026-01-29 21:37:34 +0200

    Refactor index.js to use router for CRUD operations

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const express =require('express');
-const bodyParser= require('body-parser');
-const mongoose = require('mongoose');
+const router = express.Router();
+const Coder = require('../models/coder')
 
-// Set up express app
-const app = express();
+// Get a list of coders from db
+router.get('/coders', function(req, res, next) {
+    // Coder.find({}).then(function(coders) {
+    //     res.send(coders);
+    // }).catch(next);
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost/Coder-Blooded');
-mongoose.Promise = global.Promise;
-
-app.use(bodyParser.json());
-
-// initialize the routes 
-app.use('/api',require('./routes/api'));
-
-//error handling middleware
-app.use(function(err, req, res, next){
-   //console.log(err);
-   res.status(422).send({error: err.message})
-})
-
-
-// listen for requests 
-app.listen(process.env.PORT || 3000, function(){
-console.log('now listening for requests');
+    Ninja.geoNear(
+        {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]}
+    )
 });
+
+// Add a new Coder
+router.post('/coders', function(req, res, next) {
+    Coder.create(req.body).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+
+    // res.send({
+    //    type: 'POST',
+    //    name: req.body.name,
+    //    prof: req.body.prof
+    // });
+}); // Fixed: Moved the closing brace here
+
+// Update a coder in the db
+router.put('/coders/:id', function(req, res, next) {
+    Coder.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+}); 
+
+// delete a coder from the db
+router.delete('/coders/:id', function(req, res, next) {
+    Coder.findByIdAndDelete({ _id: req.params.id }).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+    // res.send({type: 'DELETE'});
+});
+
+module.exports = router;
